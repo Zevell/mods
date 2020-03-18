86e73c724c7eaa7d3c41d96e61b2e8761762ac0b
Zeval <zevalgaming@gmail.com>
2020-03-18 19:25:23 +1000

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,4 +1,6 @@
 @echo off
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command " [System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+choco install git -y
 >git-clone.bat (
     echo @echo off
     echo RD /S /Q mods
